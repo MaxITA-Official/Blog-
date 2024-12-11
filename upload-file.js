af0c67342b2f18f8f3905bd53fbb85347eca68b0
MaxITA-Official <166740732+MaxITA-Official@users.noreply.github.com>
2024-12-11 21:28:35 +0100

    upload-file.js

--- upload-file.js
+++ upload-file.js
@@ -4,7 +4,7 @@ const formidable = require('formidable');
 
 exports.handler = async (event, context) => {
   const form = new formidable.IncomingForm();
-  form.uploadDir = path.join(__dirname, '../site/blog'); // Dove vuoi salvare il file
+  form.uploadDir = path.join(__dirname, '../'); // Salva nella root del progetto
   form.keepExtensions = true;
   form.parse(event.body, (err, fields, files) => {
     if (err) {
@@ -14,9 +14,9 @@ exports.handler = async (event, context) => {
       };
     }
 
-    const file = files.file[0]; // il file caricato
+    const file = files.file[0]; // Il file caricato
     const fileName = file.newFilename;
-    const filePath = path.join(form.uploadDir, fileName);
+    const filePath = path.join(form.uploadDir, fileName); // Salva direttamente nella root
 
     try {
       // Salva il file
